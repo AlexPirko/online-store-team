915e0f76f8a973f0efcf13bcdcf4838c15580627
Alex Pirko <youzhny_foto@gmail.com>
2022-12-15 18:09:34 +0200

    feat: add root element

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 import './style.css';
-console.log('hello·type·script');
+import { App } from './App';
+
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('The root element is undefined!');
+}
+
+const app = new App();
+
+root.innerHTML = app.render();
