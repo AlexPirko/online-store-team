6c26432cc2223d29aa29d4f2922827b958885cdc
batar-btr <btrn86@gmail.com>
2023-01-08 14:43:36 +0200

    feat: create prod build

--- src/funcs/loadImg.ts
+++
@@ -1,9 +0,0 @@
-export default function loadImage(url: string) {
-  return new Promise((resolve) => {
-    const image = new Image();
-    image.addEventListener('load', () => {
-      resolve(image);
-    });
-    image.src = url;
-  }).catch((err) => console.log('!!!!!!!!!!!!', err));
-}
--- src/funcs/makeElement.ts
+++
@@ -1,5 +0,0 @@
-export default function makeElement(elem: string, className: string): HTMLElement {
-  const element = document.createElement(elem);
-  element.classList.add(className);
-  return element;
-}
